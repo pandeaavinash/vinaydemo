33190280ad7fc9de00246cf86fb768a5334831b2
avinash <aavinashpande@gmail.com>
2021-11-28 11:03:06 +0530

    Added new files to project vinaydemo

---
+++ src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("I am in Test Class !!");
+		System.out.println("I am in Test Class !!");
+		System.out.println("I am in Test Class !!");
+		System.out.println("I am in Test Class !!");
+		System.out.println("I am in Test Class !!");
+		
+
+	}
+
+}
